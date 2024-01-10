b0743950cd9aabd32a903c834f620c6c60e01e68
Alex Widua <a.widua@gmail.com>
2024-01-10 10:17:45 +0100

    ui: refactor splash screen

--- Kami/Views/SplashView.swift
+++ Kami/Views/SplashView.swift
@@ -9,6 +9,8 @@ import KeyboardShortcuts
 }
 
 struct SplashView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismissWindow
     @AppStorage(AppStorageKey.apiKey) var appStorage_apiKey: String = ""
     @AppStorage(AppStorageKey.finishedOnboarding) var appStorage_finishedOnboarding: Bool = false
 
@@ -26,61 +28,73 @@ struct SplashView: View {
                 .navigationTitle("Enter your API Key")
         }
         else {
-            HStack(spacing: 0) {
-                ZStack{
-                    Image("SplashIconPatch")
-                    if let shortcut = KeyboardShortcuts.getShortcut(for: .toggleAppWindow) {
-                        Text(shortcut.description)
-                            .padding(4.0)
-                            .background(.white.opacity(0.2))
-                            .background(.ultraThinMaterial)
-                            .cornerRadius(6.0)
-                            .offset(x: 34, y: 14)
+            VStack(spacing: 12.0) {
+                HStack(spacing: 0) {
+                    ZStack{
+                        Image("SplashIconPatch")
+                        if let shortcut = KeyboardShortcuts.getShortcut(for: .toggleAppWindow) {
+                            Text(shortcut.description)
+                                .padding(4.0)
+                                .background(.white.opacity(0.2))
+                                .background(.ultraThinMaterial)
+                                .cornerRadius(6.0)
+                                .offset(x: 34, y: 14)
+                        }
+                    }
+                    .padding(.trailing, 24.0)
+                    VStack(alignment: .leading, spacing: 16) {
+                        Text("There are two ways to open a JavaScript patch:")
+                            .bold()
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(systemName: "a.circle.fill")
+                                    .resizable()
+                                    .foregroundStyle(.primary, .primary.opacity(0.1))
+                                    .frame(width: 16, height: 16)
+                                Text("Select a JavaScript Patch and use the shortcut **\(shortcutName)**")
+                                    .foregroundColor(.secondary)
+                            }
+                            HStack {
+                                Image(systemName: "b.circle.fill")
+                                    .resizable()
+                                    .foregroundStyle(.primary, .primary.opacity(0.1))
+                                    .frame(width: 16, height: 16)
+                                Text("Right-click a JavaScript Patch and choose **Open with...**")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding(.leading, 8.0)
+                        HStack(spacing: 4.0) {
+                            Text("You can configure the shortcut in the settings anytime.")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary.opacity(0.8))
+                        }
                     }
                 }
-                .padding(.bottom, 24.0)
-                .background(.thinMaterial)
-                VStack(alignment: .leading, spacing: 16) {
-                    Text("Open a JavaScript Patch:")
-                        .bold()
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "a.circle.fill")
-                                .resizable()
-                                .foregroundStyle(.primary, .primary.opacity(0.2))
-                                .frame(width: 16, height: 16)
-                            Text("Select a JavaScript Patch and use the shortcut **\(shortcutName)**")
-                                .foregroundColor(.secondary)
-                        }
-                        HStack {
-                            Image(systemName: "b.circle.fill")
-                                .resizable()
-                                .foregroundStyle(.primary, .primary.opacity(0.2))
-                                .frame(width: 16, height: 16)
-                            Text("Right-click a JavaScript Patch and choose **Open with...**")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    .padding(.leading, 8.0)
-                    HStack(spacing: 4.0) {
-                        Text("You can configure the shortcut in the settings.")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary.opacity(0.8))
-                        Button("Open Settings") {
-                            let _ = createSettingsWindow()
-                        }
-                        .buttonStyle(.borderless)
-                        .foregroundColor(.blue)
+                .frame(maxWidth: .infinity)
+                .padding(24.0)
+                .background {
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .fill(colorScheme == .light ? .white.opacity(0.25) : .white.opacity(0.05))
+                        .stroke(.black.opacity(0.05), lineWidth: 1)
+                }
+                HStack(alignment: .bottom) {
+                    Text("Kami is now running in the background.\nYou can quit the app anytime via the icon in the top menu bar.")
                         .font(.subheadline)
+                        .foregroundColor(.primary.opacity(0.5))
+                        .fixedSize(horizontal: false, vertical: true)
+                  Spacer()
+                    Button("Open Settings") {
+                        createSettingsWindow()
                     }
+                    Button("Got it") {
+                        dismissWindow()
+                    }
+                    .keyboardShortcut(.defaultAction)
                 }
-                .padding(.top, 8.0)
-                .padding(.horizontal, 16.0)
-                .padding(.bottom, 24.0)
             }
-            .background(.regularMaterial)
+            .padding(16.0)
             .navigationTitle(APP_NAME)
         }
-       
     }
 }
